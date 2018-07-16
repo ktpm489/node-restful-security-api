d859de1aaff996800db16fee1fb5117674df9761
vunguyen <you@example.com>
2018-07-16 23:11:02 +0700

    Find All

--- src/api/resources/song/song.controller.js
+++ src/api/resources/song/song.controller.js
@@ -1,7 +1,8 @@
 import Joi from 'joi';
-
+import Song from './song.model'
 export default {
     async create(req, res) {
+        try{
         console.log(req.body);
         const schema = Joi.object().keys({
             title: Joi.string().required(),
@@ -16,6 +17,21 @@ export default {
         if (error && error.details) {
             return res.status(400).json(error);
         }
-        return res.json(value);
+        const song = await Song.create(value)
+       // return res.json(value);
+       return res.json(song)
+    }  catch(error){
+        console.log(error)
+        return res.status(500).send(error)
+    } 
+    },
+    async findAll(req, res) {
+        try {
+            const songs = await Song.find();
+            return res.json(songs);
+        } catch (err) {
+            console.error(err);
+            return res.status(500).send(err);
+        }
     },
 };
--- src/config/db.js
+++ src/config/db.js
@@ -4,8 +4,8 @@ mongoose.Promise = global.Promise;
 //const connect = () => mongoose.connect('mongodb://data/db/music_api')
 const connect = () => mongoose.connect('mongodb://localhost/music_api', {
     socketTimeoutMS: 0,
-    keepAlive: true,
-    reconnectTries: 30
+    keepAlive: true, 
+    reconnectTries: 30,
 },function (err){
     if (err) {
         console.log('Error', err)
